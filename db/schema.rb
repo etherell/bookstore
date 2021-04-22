486e0bed5f88aeb77d0d0f3fc11d79f7d98de504
etherell <kostyaetherell@gmail.com>
2021-04-22 11:56:04 +0300

    Added tests for step 3

--- db/schema.rb
+++ db/schema.rb
@@ -96,13 +96,11 @@ ActiveRecord::Schema.define(version: 2021_04_26_191758) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
-    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
