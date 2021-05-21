2164fa569ea4f0b6d06689a8ce6fb121bf4d278c
Striapkin Kostia <60687843+etherell@users.noreply.github.com>
2021-05-21 20:58:53 +0300

    Revert "Added fixes after mentor review"
    This reverts commit 6203000bf2ba8aab0dac246fd5e810d006a9f9a3.

--- spec/support/config/controller_macros.rb
+++ spec/support/config/controller_macros.rb
@@ -2,9 +2,12 @@
 
 module ControllerMacros
   def login_user
-    @request.env['devise.mapping'] = Devise.mappings[:user]
-    user = FactoryBot.create(:user)
-    sign_in user
+    # Before each test, create and login the user
+    before do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      user = FactoryBot.create(:user)
+      sign_in user
+    end
   end
 
   # Not used in this tutorial, but left to show an example of different user types
